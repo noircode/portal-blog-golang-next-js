aece055d1e3b5c688525262eed1d0e5f52bf6dc0
aldi.maul <aldi.maulana@nawadata.id>
2024-12-29 21:27:29 +0700

    Update app.go
    Update app.go and add the main RunServer function

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,21 @@
 package app
+
+import (
+	"portal-blog/config"
+
+	"github.com/rs/zerolog/log"
+)
+
+// RunServer initializes and starts the server application.
+// It sets up the configuration, establishes a database connection,
+// and handles any errors that occur during the process.
+func RunServer() {
+	cfg := config.NewConfig()
+
+	_, err := cfg.ConnectionPostgres()
+
+	if err != nil {
+		log.Fatal().Msgf("Error connecting to database: %v", err)
+		return
+	}
+}
