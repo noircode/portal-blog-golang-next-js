597864e98e9fe05289fb7ddfe6afcfe66ded9e68
Kang Noir <93761595+noircode@users.noreply.github.com>
2025-01-12 14:45:17 +0000

    add gofiber

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,10 +1,21 @@
 package app
 
 import (
+	"context"
+	"os"
+	"os/signal"
 	"portal-blog/config"
 	"portal-blog/lib/auth"
+	"portal-blog/lib/middleware"
+	"portal-blog/lib/pagination"
+	"syscall"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/rs/zerolog/log"
 )
 
@@ -26,5 +37,39 @@ func RunServer() {
 	_ = s3.NewFromConfig(cdfR2)
 
 	_ = auth.NewJwt(cfg)
+	_ = middleware.NewMiddleware(cfg)
 
+	_ = pagination.NewPagination()
+
+	app := fiber.New()
+	app.Use(cors.New())
+	app.Use(recover.New())
+	app.Use(logger.New(logger.Config{
+		Format: "[${time}] %{ip} %{status} - %{latency} %{method} %{path}\n",
+	}))
+
+	_ = app.Group("/api")
+
+	go func() {
+		if cfg.App.AppPort == "" {
+			cfg.App.AppPort = os.Getenv("APP_PORT")
+		}
+
+		err := app.Listen(":" + cfg.App.AppPort)
+		if err != nil {
+			log.Fatal().Msgf("Error starting server: %v", err)
+    }
+	}()
+
+	quit := make(chan os.Signal, 3)
+	signal.Notify(quit, os.Interrupt)
+	signal.Notify(quit, syscall.SIGTERM)
+
+	<-quit
+
+	log.Print("server shutdown of 5 seconds\n")
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	app.ShutdownWithContext(ctx)
 }
