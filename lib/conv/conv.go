bd5d7b191244ff47dd33a6d42dcf1f6d3fb93721
Kang Noir <simaul.noir@gmail.com>
2025-03-02 12:54:33 +0700

    add StringToInt func

--- lib/conv/conv.go
+++ lib/conv/conv.go
@@ -16,8 +16,8 @@ import (
 //   - string: The bcrypt hash of the password.
 //   - error: An error if the hashing process fails, or nil if successful.
 func HashPassword(password string) (string, error) {
-    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-    return string(bytes), err
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
 }
 
 // CheckPasswordHash compares a plain-text password with a bcrypt hash.
@@ -29,8 +29,8 @@ func HashPassword(password string) (string, error) {
 // Returns:
 //   - bool: True if the password matches the hash, false otherwise.
 func CheckPasswordHash(password, hash string) bool {
-    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-    return err == nil
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
 
 // GenerateSlug creates a URL-friendly slug from a given title.
@@ -41,13 +41,27 @@ func CheckPasswordHash(password, hash string) bool {
 // Returns:
 //   - string: A lowercase string with spaces replaced by hyphens, suitable for use in URLs.
 func GenerateSlug(title string) string {
-    slug := strings.ToLower(title)
-    slug = strings.ReplaceAll(slug, " ", "-")
+	slug := strings.ToLower(title)
+	slug = strings.ReplaceAll(slug, " ", "-")
 
-    return slug
+	return slug
 }
 
-
+// StringToInt64 converts a string to an int64.
+// It takes a string as input and returns the corresponding int64 value and an error.
+// The conversion is done using base 10.
 func StringToInt64(str string) (int64, error) {
-    return strconv.ParseInt(str, 10, 64)
+	return strconv.ParseInt(str, 10, 64)
+}
+
+// StringToInt converts a string to an integer.
+// It takes a string `s` as input and returns the integer value and an error if the conversion fails.
+// If the conversion is successful, the error will be nil.
+func StringToInt(s string) (int, error) {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return 0, err
+	}
+
+	return i, nil
 }
